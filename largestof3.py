59c5e989716fb8d9cae1e1387e270faf950a7d4a
Muhammed-Ameen <73465363+Muhammed-Ameen@users.noreply.github.com>
2021-01-28 20:21:15 +0530

    Delete largestof3.py

--- largestof3.py
+++
@@ -1,6 +0,0 @@
-#find the biggest of 3 numbers
-num1=int(input("Enter first number:"))
-num2=int(input("Enter second number:"))
-num3=int(input("Enter third number"))
-big=max(num1,num2,num3)
-print("The largest number is :",big)
