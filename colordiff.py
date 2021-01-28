374830d2dcdb8dc14aa77d33f921d253a0ad8416
Muhammed-Ameen <73465363+Muhammed-Ameen@users.noreply.github.com>
2021-01-28 20:20:49 +0530

    Delete colordiff.py

--- colordiff.py
+++
@@ -1,4 +0,0 @@
-#print all colors from the list1 not contained in list2
-color_list1=set(["white","black","yellow"])
-color_list2=set(["green","blue","red"])
-print(color_list1.difference(color_list2))
