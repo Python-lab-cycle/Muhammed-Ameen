7d37ef9c3fdfc78e8f8a444bbaabae02d1694825
Muhammed-Ameen <73465363+Muhammed-Ameen@users.noreply.github.com>
2021-02-26 12:32:43 +0530

    Add files via upload

---
+++ odd line.py
@@ -0,0 +1,20 @@
+fn=open('1text.txt','r')
+
+fn1=open('odd.txt','w')
+
+content=fn.readlines()
+print("content\n",content)
+
+for i in range(0,len(content)):
+    if(i%2 !=1):
+        fn1.write(content[i])
+    else:
+       pass
+fn1.close()
+
+fn1=open('odd.txt','r')
+cont1=fn1.read()
+print("\n\n odd lines \n\n",cont1)
+
+fn.close()
+fn1.close()
